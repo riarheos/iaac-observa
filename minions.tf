136773220014b1fb4ead42e1cd00d6b3b3ecce66
Pavel Pushkarev <riarheos@gmail.com>
2024-08-06 19:57:45 +0300

    SA and minions

--- minions.tf
+++ minions.tf
@@ -1,49 +1,51 @@
-# resource "yandex_compute_instance_group" "minions-group" {
-#   name = "minions-group"
-#   service_account_id = "ajeoi0k6f2skjqem2mjg"
-#
-#   allocation_policy {
-#     zones = [var.zone]
-#   }
-#
-#   deploy_policy {
-#     max_expansion   = 1
-#     max_unavailable = 1
-#   }
-#
-#   scale_policy {
-#     fixed_scale {
-#       size = 2
-#     }
-#   }
-#
-#   instance_template {
-#     name = "minion-{instance.short_id}"
-#
-#     resources {
-#       cores  = 2
-#       memory = 1
-#       core_fraction = 5
-#     }
-#
-#     scheduling_policy {
-#       preemptible = true
-#     }
-#
-#     boot_disk {
-#       initialize_params {
-#         size = 20
-#         image_id = local.image
-#       }
-#     }
-#
-#     network_interface {
-#       subnet_ids = [yandex_vpc_subnet.subnet-a.id]
-#       nat = true
-#     }
-#
-#     metadata = {
-#       user-data = file("cloud-init.yaml")
-#     }
-#   }
-# }
+resource "yandex_compute_instance_group" "minions-group" {
+  depends_on = [yandex_compute_instance.master-vm]
+
+  name = "minions-group"
+  service_account_id = yandex_iam_service_account.paulmd-sa.id
+
+  allocation_policy {
+    zones = [var.zone]
+  }
+
+  deploy_policy {
+    max_expansion   = 1
+    max_unavailable = 1
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+
+  instance_template {
+    name = "minion-{instance.short_id}"
+
+    resources {
+      cores  = 2
+      memory = 1
+      core_fraction = 5
+    }
+
+    scheduling_policy {
+      preemptible = true
+    }
+
+    boot_disk {
+      initialize_params {
+        size = 20
+        image_id = local.image
+      }
+    }
+
+    network_interface {
+      subnet_ids = [yandex_vpc_subnet.subnet-a.id]
+      nat = true
+    }
+
+    metadata = {
+      user-data = file("cloud-init.yaml")
+    }
+  }
+}
--- sa.tf
+++ sa.tf
@@ -2,3 +2,12 @@ resource "yandex_iam_service_account" "paulmd-sa" {
   name        = "paulmd-sa"
   description = "service account to manage VMs"
 }
+
+resource "yandex_resourcemanager_folder_iam_binding" "admin" {
+  folder_id = "b1guaa1hv85esfel63i6"
+  role = "admin"
+
+  members = [
+    "serviceAccount:${yandex_iam_service_account.paulmd-sa.id}",
+  ]
+}

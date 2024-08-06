32ff9a0959dda0507febad206153b45242fec406
Pavel Pushkarev <riarheos@gmail.com>
2024-08-06 21:36:42 +0300

    Ansible done

---
+++ ansible/inventory.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+import json
+import subprocess
+
+import yaml
+
+data = subprocess.check_output(['yc', 'compute',  'instance',  'list', '--format', 'json'])
+hosts = json.loads(data)
+
+result = {}
+hostvars = {}
+for host in hosts:
+    group = host['labels']['node_type']
+    if group not in result:
+        result[group] = {
+            'hosts': [],
+        }
+    result[group]['hosts'].append(
+        host['network_interfaces'][0]['primary_v4_address']['one_to_one_nat']['address']
+    )
+    hostvars[host['network_interfaces'][0]['primary_v4_address']['one_to_one_nat']['address']] = {
+        'name': host['name'],
+        'fqdn': host['fqdn'],
+    }
+
+result['all'] = {'children': list(result.keys())}
+result['_meta'] = {'hostvars': hostvars}
+
+print(json.dumps(result))
